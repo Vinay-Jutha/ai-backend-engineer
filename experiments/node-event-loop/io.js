44e99a1d82618440df5dd49805f4c21ea646e810
Vinay <vv4250717@gmail.com>
2026-09-03 13:11:49 +0530

    experiment: experimenting with io requests

---
+++ experiments/node-event-loop/io.js
@@ -0,0 +1,21 @@
+// const fs=require("fs");
+// console.log("A");
+// fs.readFile("data.txt","utf-8",(err,data)=>{
+//     console.log("File reading finished.....");
+// });
+// Promise.resolve().then(()=>{
+//     console.log("promise resolved....");
+// });
+// setTimeout(()=>{
+//     console.log("Timer function executes....");
+// });
+// console.log("B");
+
+// console.log("A");
+// setTimeout(()=>{
+//     console.log("timer executes....");
+// });
+// setImmediate(()=>{
+//     console.log("immediate....");
+// });
+// console.log("B");
