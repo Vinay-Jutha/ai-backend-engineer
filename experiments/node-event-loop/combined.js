b13e4bad70fd09f34ee7031b384b3419040c7cfe
Vinay <vv4250717@gmail.com>
2026-09-03 13:13:34 +0530

    experiment: combining all concepts

---
+++ experiments/node-event-loop/combined.js
@@ -0,0 +1,15 @@
+// const fs=require("fs");
+// console.log("A");
+// fs.readFile("data.txt","utf8",()=>{
+//     console.log("file read successfully");
+//     Promise.resolve().then(()=>{
+//         console.log("promise resolved....");
+//     });
+//     setTimeout(()=>{ 
+//         console.log("timer function executes....")}
+//     );
+//     setImmediate(()=>{ 
+//         console.log("immediate function executes....")}
+//     );
+// });
+// console.log("B");
