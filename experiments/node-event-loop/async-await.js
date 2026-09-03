1077ad91b9dab2dffd3617ffef76ed79c24fa8ea
Vinay <vv4250717@gmail.com>
2026-09-03 16:13:39 +0530

    experiment::async-await-demo

---
+++ experiments/node-event-loop/async-await.js
@@ -0,0 +1,16 @@
+// function getData(){
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             return resolve("database completed");
+//         },3000);
+//     });
+// }
+// async function request(){
+//     console.log("request 1 started.....");
+//     const data=await getData();
+//     console.log("promise resolved..");
+//     console.log("data collected....");
+// }
+// console.log("start");
+// request();
+// console.log("end");
